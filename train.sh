d71af4f62e324244607a38d25e161a5614cda666
kantakaneda <k.kaneda@keio.jp>
2022-01-29 11:15:44 +0900

    [ID25] k=2,4,8,12,24

--- train.sh
+++ train.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# python train.py --params params/params_without_pretrain_2017.json --wandb
+# python train.py --params params/params_without_pretrain_2016.json --wandb
+# python train.py --params params/params_without_pretrain_2015.json --wandb
+# python train.py --params params/params_without_pretrain_2014.json --wandb
+
 python train.py --params params/params_without_pretrain_2017.json --wandb
-python train.py --params params/params_without_pretrain_2016.json --wandb
-python train.py --params params/params_without_pretrain_2015.json --wandb
-python train.py --params params/params_without_pretrain_2014.json --wandb
+python train.py --params params/params_k=4.json --wandb
+python train.py --params params/params_k=8.json --wandb
+python train.py --params params/params_k=12.json --wandb
+python train.py --params params/params_k=24.json --wandb
