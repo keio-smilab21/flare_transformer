b3dcf63e4984f0d4e12f827fc35a61b4dacb62e8
kantakaneda <k.kaneda@keio.jp>
2022-01-29 11:19:03 +0900

    [ID25] before BSS fix

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python train.py --params params/params_without_pretrain_2017.json --wandb
+# python train.py --params params/params_without_pretrain_2017.json --wandb
 python train.py --params params/params_without_pretrain_2016.json --wandb
 python train.py --params params/params_without_pretrain_2015.json --wandb
 python train.py --params params/params_without_pretrain_2014.json --wandb
