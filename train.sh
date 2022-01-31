b0c8ef2a281278de0a6622ae8e94aa06761390bb
kantakaneda <k.kaneda@keio.jp>
2022-01-31 10:27:45 +0900

    [ID40] params

--- train.sh
+++ train.sh
@@ -4,8 +4,12 @@
 # python train.py --params params/params_without_pretrain_2015.json --wandb
 # python train.py --params params/params_without_pretrain_2014.json --wandb
 
-python train.py --params params/params_k=2.json --wandb
-python train.py --params params/params_k=4.json --wandb
-python train.py --params params/params_k=8.json --wandb
+# python train.py --params params/params_k=2.json --wandb
+# python train.py --params params/params_k=4.json --wandb
+# python train.py --params params/params_k=8.json --wandb
 # python train.py --params params/params_k=12.json --wandb
 # python train.py --params params/params_k=24.json --wandb
+
+python train.py --params params/params_without_pretrain_2017.json --wandb
+python train.py --params params/params_id40-e2.json --wandb
+python train.py --params params/params_without_pretrain_2016.json --wandb
