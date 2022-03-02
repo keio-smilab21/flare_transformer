053bd7bd01e40f6be1a1efa698df5d651d4411bd
kantakaneda <k.kaneda@keio.jp>
2022-03-02 15:23:19 +0900

    [CHANGE] 不要なBSSを削除

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -1,14 +1,13 @@
 """Scripts for evaluation metrics"""
 
-import sys
 import math
 from sklearn import metrics
 import numpy as np
 
 
-def calc_score(y_pred, y_true, climatology, climatology_c):
+def calc_score(y_pred, y_true, climatology):
     """
-    Compute acc, TSS, BSS, and GMGS
+    Compute ACC, TSS, BSS, and GMGS
     """
     score = {}
     y_predl = []
@@ -18,9 +17,6 @@ def calc_score(y_pred, y_true, climatology, climatology_c):
     score["ACC"] = calc_acc4(y_predl, y_true)
     score["TSS-M"] = calc_tss(y_predl, y_true, 2)
     score["BSS-M"] = calc_bss(y_pred, y_true, climatology)
-    # score["BSS-X"] = calc_bss_x(y_pred, y_true)
-    # score["BSS-C"] = calc_bss_c(y_pred, y_true, climatology_c)
-    # score["BSS"] = calc_bss_4(y_pred, y_true)
     score["GMGS"] = calc_gmgs(y_predl, y_true)
 
     return score
@@ -52,26 +48,6 @@ def calc_gmgs(y_predl, y_true):
     return (tss_x + tss_m + tss_c) / 3
 
 
-# def calc_bss(y_pred, y_true, climatology):  # backup
-#     """
-#     Compute BSS >= M
-#     """
-#     y_truel = []
-#     for y in y_true:
-#         y_truel.append(convert_2_one_hot_2class(y))
-
-#     y_pred2 = np.reshape(np.array(y_pred).ravel(), (-1, 2))
-#     y_pred2 = np.sum(y_pred2, axis=1)
-#     bs = 0
-#     bsc = 0
-#     for p, t in zip(y_pred2.tolist(),
-#                     np.array(y_truel).ravel().tolist()):
-#         bs += (p - t) ** 2
-#     for f, y in zip(climatology*len(y_true), np.array(y_truel).ravel().tolist()):
-#         bsc += (f - y) ** 2
-#     bss = (bsc - bs) / bsc
-#     return bss
-
 def calc_bss(y_pred, y_true, climatology):
     """
     Compute BSS >= M
@@ -82,22 +58,14 @@ def calc_bss(y_pred, y_true, climatology):
 
     y_pred2 = np.reshape(np.array(y_pred).ravel(), (-1, 2))
     y_pred2 = np.reshape(np.sum(y_pred2, axis=1), (-1, 2))
-    # print(y_pred2, y_pred2.shape)
     bs = 0
     bsc = 0
     for p, t in zip(y_pred2.tolist(),
                     np.array(y_truel).tolist()):
-        # print(p, t)
         bs += (p[1] - t[1]) ** 2
-        # print(bs)
-        # sys.exit()
-    # for f, y in zip(climatology*len(y_true), np.array(y_truel).ravel().tolist()):
-    #     bsc += (f - y) ** 2
     bs = bs / len(y_true)
     bsc = climatology[0] * climatology[1]
     bss = (bsc - bs) / bsc
-    # print(bs, len(y_true), bsc, bss)
-    # sys.exit()
 
     return bss
 
@@ -149,101 +117,3 @@ def calc_tp_4(four_class_matrix, flare_class):
                 else:
                     tn += four_class_matrix[true][pred]
     return tn, fp, fn, tp
-
-
-def calc_bss_4(y_pred, y_true):
-    """
-    Compute BSS
-    """
-    climatology = [0.5405, 0.3648, 0.0861, 0.0086]
-    y_truel = []
-    for y in y_true:
-        y_truel.append(convert_2_one_hot(y))
-
-    bs = 0
-    bsc = 0
-    for p, t in zip(np.array(y_pred).ravel().tolist(),
-                    np.array(y_truel).ravel().tolist()):
-        bs += (p - t) ** 2
-    for f, y in zip(climatology*len(y_true), np.array(y_truel).ravel().tolist()):
-        bsc += (f - y) ** 2
-    bss = (bsc - bs) / bsc
-    return bss
-
-
-def calc_bss_x(y_pred, y_true):
-    """
-    Compute BSS >= X
-    """
-    climatology = [0.99, 0.01]
-    y_truel = []
-    for y in y_true:
-        y_truel.append(convert_2_one_hot_Xclass(y))
-
-    y_pred_x = []
-    for py in y_pred:
-        y_pred_x.append([np.sum(py[:3]), py[-1]])
-    y_pred_x = np.array(y_pred_x).ravel()
-    bs = 0
-    bsc = 0
-    for p, t in zip(y_pred_x.tolist(),
-                    np.array(y_truel).ravel().tolist()):
-        bs += (p - t) ** 2
-    for f, y in zip(climatology*len(y_true), np.array(y_truel).ravel().tolist()):
-        bsc += (f - y) ** 2
-    bss = (bsc - bs) / bsc
-    return bss
-
-
-def convert_2_one_hot_Xclass(binary_value):
-    """
-    return 2-dimentional 1-of-K vector
-    """
-    if binary_value == 3:
-        return [0, 1]
-    return [1, 0]
-
-
-def calc_bss_c(y_pred, y_true, climatology_c):
-    """
-    Compute BSS >= X
-    """
-    y_truel = []
-    for y in y_true:
-        y_truel.append(convert_2_one_hot_Cclass(y))
-
-    y_pred_c = []
-    for py in y_pred:
-        y_pred_c.append([py[0], np.sum(py[1:])])
-    y_pred_c = np.array(y_pred_c).ravel()
-    bs = 0
-    bsc = 0
-    for p, t in zip(y_pred_c.tolist(),
-                    np.array(y_truel).ravel().tolist()):
-        bs += (p - t) ** 2
-    for f, y in zip(climatology_c*len(y_true), np.array(y_truel).ravel().tolist()):
-        bsc += (f - y) ** 2
-    bss = (bsc - bs) / bsc
-    return bss
-
-
-def convert_2_one_hot_Cclass(binary_value):
-    """
-    return 2-dimentional 1-of-K vector
-    """
-    if binary_value == 0:
-        return [1, 0]
-    return [0, 1]
-
-
-def convert_2_one_hot(binary_value):
-    """
-    return four-dimentional 1-of-K vector
-    """
-    if binary_value == 3:
-        return [0, 0, 0, 1]
-    if binary_value == 2:
-        return [0, 0, 1, 0]
-    if binary_value == 1:
-        return [0, 1, 0, 0]
-    return [1, 0, 0, 0]
