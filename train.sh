adef663578274030ab777d813ad1de019dd0a97e
kantakaneda <k.kaneda@keio.jp>
2022-03-06 15:24:22 +0900

    [ID59] Informer+EF

--- train.sh
+++ train.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-python train.py --params params/params_2017.json
+python train.py --params params/params_2014.json --wandb
+python train.py --params params/params_2017.json --wandb
+python train.py --params params/params_2016.json --wandb
+python train.py --params params/params_2015.json --wandb
